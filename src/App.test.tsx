142431b57e2340986976119c8909f44440d82d17
georgyshkliaryk <h.shkliaryk@gmail.com>
2021-11-09 22:20:04 +0300

    config for eslint created, new dependencies installed

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders hello", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getByText(/hello/i);
+  expect(element).toBeInTheDocument();
 });
