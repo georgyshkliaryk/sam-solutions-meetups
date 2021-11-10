98fc97f00b7d9404f2801d4f5986172bba84f80c
georgyshkliaryk <h.shkliaryk@gmail.com>
2021-11-10 14:55:03 +0300

    added header component; added mixins in scss

---
+++ src/components/header/HeaderContainer/HeaderContainer.tsx
@@ -0,0 +1,8 @@
+import React, { ReactElement } from "react";
+import "./HeaderContainer.scss";
+
+const HeaderContainer: React.FC = (): ReactElement => {
+  return <div className="header"></div>;
+};
+
+export default HeaderContainer;
--- src/pages/meetups/themes/ThemesPage.tsx
+++ src/pages/meetups/themes/ThemesPage.tsx
@@ -1,7 +1,13 @@
 import React, { ReactElement } from "react";
+import HeaderContainer from "../../../components/header/HeaderContainer/HeaderContainer";
 
 const ThemesPage: React.FC = (): ReactElement => {
-  return <div>Themes Page</div>;
+  return (
+    <div>
+      <HeaderContainer />
+      <div>Themes Page</div>
+    </div>
+  );
 };
 
 export default ThemesPage;
