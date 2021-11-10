5456aaf293f0c6f5e17346181e237270b2a56f7d
georgyshkliaryk <h.shkliaryk@gmail.com>
2021-11-10 13:15:29 +0300

    deleted eslint from dependencies, added precommit hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { ReactElement } from "react";
 import "./App.scss";
 
 const App: React.FC = (): ReactElement => {
-  return <div className="App">Hello world     </div>;
+  return <div className="App">Hello world</div>;
 };
 
 export default App;
