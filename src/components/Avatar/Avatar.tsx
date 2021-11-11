e0fa428941ee2e6f239acb1c89d63fe81e6b994c
georgyshkliaryk <h.shkliaryk@gmail.com>
2021-11-11 21:06:54 +0300

    pass image as props to Avatar component

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -2,10 +2,18 @@ import React, { ReactElement } from "react";
 import "./Avatar.scss";
 import AvatarImg from "./assets/avatar.svg";
 
-const Avatar: React.FC = (): ReactElement => {
+interface IProps {
+  img?: string;
+}
+
+const Avatar: React.FC<IProps> = (props): ReactElement => {
   return (
     <div className="avatar-wrapper">
-      <img src={AvatarImg} alt="user's avatar" />
+      <img
+        src={props.img ? props.img : AvatarImg} // if user has avatar, display it; else display standard one
+        alt="user's avatar"
+        className="avatar-wrapper__img"
+      />
     </div>
   );
 };
