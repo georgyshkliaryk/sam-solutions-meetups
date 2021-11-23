c2b04aee90807a5c4c2c7130eec069227b469745
georgyshkliaryk <h.shkliaryk@gmail.com>
2021-11-24 01:25:06 +0300

    added hook to disable submit button if username or password is empty

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,7 +1,19 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useState } from "react";
 import "./LoginPage.scss";
 
 const LoginPage: React.FC = (): ReactElement => {
+  const loginRef = React.createRef<HTMLInputElement>();
+  const passwordRef = React.createRef<HTMLInputElement>();
+  const [isSubmitDisabled, setIsSubmitDisabled] = useState(true);
+
+  const handleInputChange = () => {
+    if (loginRef.current?.value === "" || passwordRef.current?.value === "") {
+      setIsSubmitDisabled(true);
+    } else {
+      setIsSubmitDisabled(false);
+    }
+  };
+
   return (
     <div className="login-page">
       <form className="login-page-container">
@@ -17,6 +29,8 @@ const LoginPage: React.FC = (): ReactElement => {
             className="login-page-container__input"
             placeholder="Логин"
             id="login-input"
+            ref={loginRef}
+            onChange={handleInputChange}
           />
         </div>
         <div className="login-page-container__input-wrapper">
@@ -31,12 +45,14 @@ const LoginPage: React.FC = (): ReactElement => {
             className="login-page-container__input"
             placeholder="Пароль"
             id="password-input"
+            ref={passwordRef}
+            onChange={handleInputChange}
           />
         </div>
         <button
           className="login-page-container__submit-button"
           type="submit"
-          disabled
+          disabled={isSubmitDisabled}
         >
           Войти <span className="material-icons-round">login</span>
         </button>
