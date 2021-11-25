7a03b65042f57a3de7f34fb5574063c0f23715aa
georgyshkliaryk <h.shkliaryk@gmail.com>
2021-11-25 11:44:12 +0300

    small routes fix

--- src/components/PrivateRoute/PrivateRoute.tsx
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -2,11 +2,14 @@ import React, { ReactElement, useContext } from "react";
 import { StoreContext } from "../../context/StoreContext";
 import { Navigate } from "react-router-dom";
 import { observer } from "mobx-react-lite";
+import { routes } from "../../constants";
 
 const PrivateRoute: React.FC = observer(({ children }): ReactElement => {
   const { authStore } = useContext(StoreContext);
   return (
-    <>{authStore.isAuthenticated() ? children : <Navigate to="/login" />}</>
+    <>
+      {authStore.isAuthenticated() ? children : <Navigate to={routes.login} />}
+    </>
   );
 });
 
